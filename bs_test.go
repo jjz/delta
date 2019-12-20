d027567bbcdeef91448859ed81d1616d77ab1905
jjz <woaf1003@gmail.com>
2019-12-20 12:00:20 +0800

    option put  unit test

--- bs_test.go
+++ bs_test.go
@@ -5,21 +5,12 @@ import (
 	"testing"
 )
 
-func TestNewOption(t *testing.T) {
+func TestCallOption(t *testing.T) {
 	S0 := 50.0
-
-	// K := 640.0
-	// right := "P"
-	// price := 2.4
-
 	K := 100.0
 	right := "C"
 	price := -1.0
-
-	// vol := 0.2939
-	// price := -1.0
 	vol := 0.25
-
 	r := 0.05 // risk free rate
 	eval_date := "20150115"
 	exp_date := "20160115"
@@ -34,4 +25,25 @@ func TestNewOption(t *testing.T) {
 	assert.Equal(t, opt.gamma, 0.004001548287317137,"gamma")
 	assert.Equal(t, opt.sigma, 0.25,"sigma")
 
+}
+
+func TestPutOption(t *testing.T){
+	S0 := 50.0
+	K := 60.0
+	right := "P"
+	price := -1.0
+	vol := 0.25
+	r := 0.05 // risk free rate
+	eval_date := "20150115"
+	exp_date := "20160115"
+
+	opt := NewOption(right, S0, K, eval_date, exp_date, r, vol, price)
+
+	assert.Equal(t, opt.right, "P","option rigth")
+	assert.Equal(t, opt.T, 1.0,"option time")
+	assert.Equal(t, opt.price, 9.586472902449302,"option price")
+	assert.Equal(t, opt.delta, -0.656998882557075,"delta")
+	assert.Equal(t, opt.theta, -3.637720165611989,"theta")
+	assert.Equal(t, opt.gamma, 0.07372212501922741,"gamma")
+	assert.Equal(t, opt.sigma, 0.25,"sigma")
 }
--- main.go
+++ main.go
@@ -21,6 +21,7 @@ func main() {
 	//}
 
 	bstest()
+	bstput()
 }
 
 func bstest() {
@@ -45,6 +46,38 @@ func bstest() {
 	opt := NewOption(right, S0, K, eval_date, exp_date, r, vol, price)
 
 	log.Println("CALL")
+	log.Printf("T: %v\n", opt.T)
+	log.Printf("Price: %v\n", opt.price)
+	log.Printf("Delta: %v\n", opt.delta)
+	log.Printf("Theta: %v\n", opt.theta)
+	log.Printf("Gamma: %v\n", opt.gamma)
+	log.Printf("Volatility: %v\n", opt.sigma)
+}
+
+
+
+func bstput() {
+	S0 := 50.0
+
+	// K := 640.0
+	// right := "P"
+	// price := 2.4
+
+	K := 60.0
+	right := "P"
+	price := -1.0
+
+	// vol := 0.2939
+	// price := -1.0
+	vol := 0.25
+
+	r := 0.05 // risk free rate
+	eval_date := "20150115"
+	exp_date := "20160115"
+
+	opt := NewOption(right, S0, K, eval_date, exp_date, r, vol, price)
+
+	log.Println("PUT")
 	log.Printf("T: %v\n", opt.T)
 	log.Printf("Price: %v\n", opt.price)
 	log.Printf("Delta: %v\n", opt.delta)
